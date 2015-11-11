e64a62d9cbbc0486ca84b362263c06154014280c
Viktor Zavala <viktor@acklenavenue.com>
2015-11-11 01:48:18 -0600

    made small changes to home.html

--- client/app/scripts/controllers/HomeController.js
+++ client/app/scripts/controllers/HomeController.js
@@ -1,4 +1,9 @@
 angular.module('AngularScaffold.Controllers')
   .controller('HomeController', ['$scope', function ($scope) {
     	$scope.title = "Hola, mundo."
+      $scope.exampleObject = {text: "Hola, Mundo"}
+
+      $scope.changeExampleObject = function(){
+        $scope.exampleObject = {text: "Adios, mundo."};
+      }
   }]);
