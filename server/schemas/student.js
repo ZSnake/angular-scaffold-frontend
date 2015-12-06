f43d4980c2d761a2f9d36c0c78cfb15ae7decee3
viktor <viktor@acklenavenue.com>
2015-12-06 02:59:34 -0600

    removed everything backend related

--- server/schemas/student.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var StudentSchema = new mongoose.Schema({
-  name : String,
-  account : String,
-});
-
-module.exports = mongoose.model('Student', StudentSchema);
